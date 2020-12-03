1a571ba5ffcda729c2a9f3234baf857af4242bca
Connor May <poiu2463@gmail.com>
2020-12-03 10:27:03 -0500

    More work on navgraph and recyclerview

--- source/android_app/app/src/main/java/com/example/startrack/Fragments/CargoRunDetailFragment.kt
+++ source/android_app/app/src/main/java/com/example/startrack/Fragments/CargoRunDetailFragment.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
 import com.example.startrack.R
 
 /**
@@ -19,7 +17,7 @@ class CargoRunDetailFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.cargo_run_detail, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- source/android_app/app/src/main/java/com/example/startrack/Fragments/CargoRunListFragment.kt
+++ source/android_app/app/src/main/java/com/example/startrack/Fragments/CargoRunListFragment.kt
@@ -19,14 +19,12 @@ class CargoRunListFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.cargo_run_list, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+
     }
 }
